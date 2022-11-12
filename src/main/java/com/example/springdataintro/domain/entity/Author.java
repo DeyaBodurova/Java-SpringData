a60082811c746c93a266815441ab3fc57b72cc4b
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-11-12 14:41:25 +0200

    wip

--- src/main/java/com/example/springdataintro/domain/entity/Author.java
+++ src/main/java/com/example/springdataintro/domain/entity/Author.java
@@ -5,10 +5,7 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.Column;
-import javax.persistence.Entity;
-import javax.persistence.OneToMany;
-import javax.persistence.Table;
+import javax.persistence.*;
 import java.util.Set;
 
 @Getter
@@ -25,6 +22,8 @@ public class Author extends BaseEntity {
     @Column(name = "last_name", nullable = false)
     private String lastName;
 
-    @OneToMany
+    @OneToMany(targetEntity = Book.class,
+            mappedBy = "author",
+            fetch = FetchType.EAGER)
     private Set<Book> books;
 }
