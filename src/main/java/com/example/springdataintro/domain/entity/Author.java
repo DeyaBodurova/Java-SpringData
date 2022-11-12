470110cc90c8d0a9b1cc5b259c9c0bc584e0ee16
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-11-12 14:31:56 +0200

    wip

--- src/main/java/com/example/springdataintro/domain/entity/Author.java
+++ src/main/java/com/example/springdataintro/domain/entity/Author.java
@@ -5,14 +5,21 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Table;
+
 @Getter
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
 @Entity
 @Table(name = "authors")
-public class Author extends BaseEntity{
+public class Author extends BaseEntity {
 
+    @Column(name = "first_name")
+    private String firstName;
+
+    @Column(name = "last_name", nullable = false)
+    private String lastName;
 }
