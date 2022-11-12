a800478a2c3cb744fbdb51703427ddd2dad632e0
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-11-12 14:58:04 +0200

    wip

---
+++ src/main/java/com/example/springdataintro/domain/enums/FilePath.java
@@ -0,0 +1,15 @@
+package com.example.springdataintro.domain.enums;
+
+public enum FilePath {
+
+    ;
+
+    public static final String RESOURCE_URL = "src/main/resources/dbConnect/";
+
+    public static final String AUTHORS_FILE_NAME = "authors.txt";
+
+    public static final String BOOKS_FILE_NAME = "books.txt";
+
+    public static final String CATEGORIES_FILE_NAME = "categories.txt";
+
+}
