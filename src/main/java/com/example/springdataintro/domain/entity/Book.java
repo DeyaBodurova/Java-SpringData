65c876e1fc70998f44c5628fdc26e250105713b8
Deya Bodurova <102071694+DeyaBodurova@users.noreply.github.com>
2022-11-12 14:29:31 +0200

    wip

--- src/main/java/com/example/springdataintro/domain/entity/Book.java
+++ src/main/java/com/example/springdataintro/domain/entity/Book.java
@@ -1,12 +1,14 @@
 package com.example.springdataintro.domain.entity;
 
+import com.example.springdataintro.domain.enums.AgeRestriction;
+import com.example.springdataintro.domain.enums.EditionType;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.time.LocalDate;
 
 @Getter
 @Setter
@@ -15,8 +17,26 @@ import javax.persistence.Table;
 @Entity
 @Table(name = "books")
 public class Book extends BaseEntity {
+
+    @Column(nullable = false, length = 50)
     private String title;
+
+    @Column(nullable = false, length = 1000)
     private String description;
-    private String editionType;
+
+    @Enumerated(EnumType.STRING)
+    private EditionType editionType;
+
+    @Column(nullable = false)
+    private Double price;
+
+    @Column(nullable = false)
+    private Integer copies;
+
+    @Column(name = "release_date")
+    private LocalDate releaseDate;
+
+    @Enumerated(EnumType.STRING)
+    private AgeRestriction ageRestriction;
 
 }
